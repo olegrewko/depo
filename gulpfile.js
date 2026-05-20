7ee3440e62465300858b264b6660af6ed1d354ff
olegrewko <olegrewko@gmail.com>
2026-05-20 11:36:07 +0300

    rt300

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 const gulp = require('gulp');
-const cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
 const del = require('del');
@@ -11,30 +10,57 @@ const path = require('path');
 const clean = () => del(['dist/*']);
 
 // Функция для встраивания CSS и JS в HTML
+function inlineAssets() {
+  const Transform = require('stream').Transform;
 
-// В функции inlineAssets замените обработку CSS на эту:
-contents = contents.replace(/<link rel="stylesheet" href="([^"]+\.css)">/g, (match, cssPath) => {
-  try {
-    const fullPath = path.join('docs', cssPath);
-    if (fs.existsSync(fullPath)) {
-      let cssContent = fs.readFileSync(fullPath, 'utf8');
-      // НЕ минифицируем CSS, чтобы не сломать медиа-запросы
-      return `<style>${cssContent}</style>`;
+  return new Transform({
+    objectMode: true,
+    transform(file, enc, callback) {
+      if (file.isBuffer() && path.extname(file.path) === '.html') {
+        let contents = file.contents.toString();
+
+        // Встраиваем CSS файлы
+        contents = contents.replace(/<link rel="stylesheet" href="([^"]+\.css)">/g, (match, cssPath) => {
+          try {
+            const fullPath = path.join('docs', cssPath);
+            if (fs.existsSync(fullPath)) {
+              let cssContent = fs.readFileSync(fullPath, 'utf8');
+              return `<style>${cssContent}</style>`;
+            }
+          } catch (e) {
+            console.log(`Ошибка при встраивании CSS: ${cssPath}`);
+          }
+          return match;
+        });
+
+        // Встраиваем JS файлы
+        contents = contents.replace(/<script src="([^"]+\.js)"><\/script>/g, (match, jsPath) => {
+          try {
+            const fullPath = path.join('docs', jsPath);
+            if (fs.existsSync(fullPath)) {
+              let jsContent = fs.readFileSync(fullPath, 'utf8');
+              return `<script>${jsContent}</script>`;
+            }
+          } catch (e) {
+            console.log(`Ошибка при встраивании JS: ${jsPath}`);
+          }
+          return match;
+        });
+
+        file.contents = Buffer.from(contents);
+      }
+      callback(null, file);
     }
-  } catch (e) {
-    console.log(`Ошибка при встраивании CSS: ${cssPath}`);
-  }
-  return match;
-});
+  });
+}
+
 // Обработка HTML с встраиванием
 function html() {
   return gulp.src('docs/*.html')
     .pipe(inlineAssets())
     .pipe(htmlmin({
       collapseWhitespace: true,
-      removeComments: true,
-      minifyCSS: true,
-      minifyJS: true
+      removeComments: true
     }))
     .pipe(gulp.dest('dist'))
     .pipe(browserSync.stream());
